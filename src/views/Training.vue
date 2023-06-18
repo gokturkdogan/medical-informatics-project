912cef209057fc36017b3fb812edcaee875caf5c
gokturk.dogan <gokturk.dogan@hotmail.com>
2023-06-18 20:04:42 +0300

    training-page training page dynamic

--- src/views/Training.vue
+++ src/views/Training.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="training">
-        <Banner />
-        <LastTraining />
-    </div>
-  </template>
+  <div class="training">
+    <Banner />
+    <LastTraining :trainings="trainings"/>
+  </div>
+</template>
   
   <script>
-  import Banner from '../components/training/Banner.vue';
-  import LastTraining from '../components/training/LastTraining.vue';
-  export default {
-    name: "Training",
-    components: {
-        Banner,
-        LastTraining
+import Banner from "../components/training/Banner.vue";
+import LastTraining from "../components/training/LastTraining.vue";
+export default {
+  name: "Training",
+  components: {
+    Banner,
+    LastTraining,
+  },
+  created() {
+    this.$store.dispatch("training/fetchTrainings");
+  },
+  computed: {
+    trainings() {
+      return this.$store.getters["training/getTrainingData"];
     },
-    created() {
-        this.$store.dispatch('training/fetchTrainings')
-    }
-  };
-  </script>
+  },
+};
+</script>
